53155114c01688c67639ae0a2ee4c1084b734d75
Nagaraju0123 <141734111+Nagaraju0123@users.noreply.github.com>
2024-05-19 17:03:21 +0530

    Create About.js

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import { FcAbout } from "react-icons/fc";
+import './About.css'
+
+const About = () => {
+    return (
+        <div className="about-container">
+            <FcAbout className="about" />
+            <h1 className="about-heading">About</h1>
+        </div>
+    )
+}
+export default About
